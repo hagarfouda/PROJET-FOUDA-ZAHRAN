6ba5f456b789f4d8f79779c788913c73ebf3f8c2
Hagar Fouda <hagarfouda@hotmail.fr>
2019-03-26 15:46:21 +0100

    Initial commit

---
+++ DONIA ZAHRAN & HAGAR FOUDA /Visualisation.js
@@ -0,0 +1,329 @@
+function Visualisation1() {
+  var nature = document.getElementById("Nature");/* On appelle les elements grace à leurs ID*/
+  var test = document.getElementById("test");
+  if (nature.checked == true){ /* On  verifie si la case est cocher*/
+    test.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation2() {
+  var choc = document.getElementById("Chocolat");/* On appelle les elements grace à leurs ID*/
+  var test1 = document.getElementById("test1");
+  if (choc.checked == true){ /* On  verifie si la case est cocher*/
+    test1.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test1.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+
+function Visualisation3() {
+  var Spis = document.getElementById("Spis");/* On appelle les elements grace à leurs ID*/
+  var test2 = document.getElementById("test2");
+  var Schoc = document.getElementById("Schoc");/* On appelle les elements grace à leurs ID*/
+  if (Spis.checked == true || Schoc.checked == true){ /* On  verifie si la case est cocher*/
+    test2.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test2.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+
+function Visualisation4() {
+  var Pchoc = document.getElementById("Pchoc");/* On appelle les elements grace à leurs ID*/
+  var test4 = document.getElementById("test4");
+  if (Pchoc.checked == true){ /* On  verifie si la case est cocher*/
+    test4.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test4.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation5() {
+  var PCBS = document.getElementById("PCBS");/* On appelle les elements grace à leurs ID*/
+  var test5 = document.getElementById("test5");
+  if (PCBS.checked == true){ /* On  verifie si la case est cocher*/
+    test5.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test5.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+
+function Visualisation6() {
+  var GCB = document.getElementById("GCB");/* On appelle les elements grace à leurs ID*/
+  var test6 = document.getElementById("test6");
+  if (GCB.checked == true){ /* On  verifie si la case est cocher*/
+    test6.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test6.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation7() {
+  var GC = document.getElementById("GC");/* On appelle les elements grace à leurs ID*/
+  var test7 = document.getElementById("test7");
+  if (GC.checked == true){ /* On  verifie si la case est cocher*/
+    test7.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test7.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+function Visualisation8() {
+  var oreo = document.getElementById("O");/* On appelle les elements grace à leurs ID*/
+  var test8 = document.getElementById("test8");
+  if (oreo.checked == true){ /* On  verifie si la case est cocher*/
+    test8.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test8.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation9() {
+  var daim = document.getElementById("D");/* On appelle les elements grace à leurs ID*/
+  var test9 = document.getElementById("test9");
+  if (daim.checked == true){ /* On  verifie si la case est cocher*/
+    test9.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test9.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+
+
+/*Creme fruitee*/
+
+function Visualisation10() {
+  var fdp = document.getElementById("c1");/* On appelle les elements grace à leurs ID*/
+  var test10 = document.getElementById("test10");
+  if (fdp.checked == true){ /* On  verifie si la case est cocher*/
+    test10.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test10.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation11() {
+  var fr = document.getElementById("c2");/* On appelle les elements grace à leurs ID*/
+  var test11 = document.getElementById("test11");
+  if (fr.checked == true){ /* On  verifie si la case est cocher*/
+    test11.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test11.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+
+function Visualisation12() {
+  var MC = document.getElementById("mchoc");/* On appelle les elements grace à leurs ID*/
+  var test12 = document.getElementById("test12");
+  if (MC.checked == true){ /* On  verifie si la case est cocher*/
+    test12.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test12.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation13() {
+  var fe = document.getElementById("fe");/* On appelle les elements grace à leurs ID*/
+  var test13 = document.getElementById("test13");
+  if (fe.checked == true){ /* On  verifie si la case est cocher*/
+    test13.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test13.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+
+function Visualisation14() {
+  var bv = document.getElementById("bavaroise");/* On appelle les elements grace à leurs ID*/
+  var test14 = document.getElementById("test14");
+  if (bv.checked == true){ /* On  verifie si la case est cocher*/
+    test14.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test14.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation15() {
+  var ptf = document.getElementById("ptf");/* On appelle les elements grace à leurs ID*/
+  var test15 = document.getElementById("test15");
+  if (ptf.checked == true){ /* On  verifie si la case est cocher*/
+    test15.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test15.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation16() {
+  var royal = document.getElementById("royal");/* On appelle les elements grace à leurs ID*/
+  var test16 = document.getElementById("test16");
+  if (royal.checked == true){ /* On  verifie si la case est cocher*/
+    test16.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test16.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation17() {
+  var miroir = document.getElementById("miroir");/* On appelle les elements grace à leurs ID*/
+  var test17 = document.getElementById("test17");
+  if (miroir.checked == true){ /* On  verifie si la case est cocher*/
+    test17.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test17.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation18() {
+  var americain = document.getElementById("americain");/* On appelle les elements grace à leurs ID*/
+  var test18 = document.getElementById("test18");
+  if (americain.checked == true){ /* On  verifie si la case est cocher*/
+    test18.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test18.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation19() {
+  var pbeurre = document.getElementById("pbeurre");/* On appelle les elements grace à leurs ID*/
+  var test19 = document.getElementById("test19");
+  if (pbeurre.checked == true){ /* On  verifie si la case est cocher*/
+    test19.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test19.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+/*decors*/
+
+function Visualisation21() {
+  var petoiles = document.getElementById("petoiles");/* On appelle les elements grace à leurs ID*/
+  var test20 = document.getElementById("test20");
+  if (petoiles.checked == true){ /* On  verifie si la case est cocher*/
+    test20.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test20.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+function Visualisation20() {
+  var vermicelles = document.getElementById("vermicelles");/* On appelle les elements grace à leurs ID*/
+  var test21 = document.getElementById("test21");
+  if (vermicelles.checked == true){ /* On  verifie si la case est cocher*/
+    test21.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test21.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation22() {
+  var mangua = document.getElementById("mangua");/* On appelle les elements grace à leurs ID*/
+  var test22 = document.getElementById("test22");
+  if (mangua.checked == true){ /* On  verifie si la case est cocher*/
+    test22.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test22.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation23() {
+  var dfr = document.getElementById("dfr");/* On appelle les elements grace à leurs ID*/
+  var test23 = document.getElementById("test23");
+  if (dfr.checked == true){ /* On  verifie si la case est cocher*/
+    test23.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test23.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation25() {
+  var tagada = document.getElementById("ourson");/* On appelle les elements grace à leurs ID*/
+  var test24 = document.getElementById("test24");
+  if (ourson.checked == true){ /* On  verifie si la case est cocher*/
+    test24.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test24.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation24() {
+  var tagada = document.getElementById("tagada");/* On appelle les elements grace à leurs ID*/
+  var test25 = document.getElementById("test25");
+  if (tagada.checked == true){ /* On  verifie si la case est cocher*/
+    test25.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test25.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+function Visualisation26() {
+  var kdr = document.getElementById("kinder");/* On appelle les elements grace à leurs ID*/
+  var test26 = document.getElementById("test26");
+  if (kdr.checked == true){ /* On  verifie si la case est cocher*/
+    test26.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test26.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+function Visualisation27() {
+  var kitkat = document.getElementById("kk");/* On appelle les elements grace à leurs ID*/
+  var test27 = document.getElementById("test27");
+  if (kitkat.checked == true){ /* On  verifie si la case est cocher*/
+    test27.style.display = "inline-block"; /* affiche l'image*/
+  }
+  else {
+    test27.style.display = "none"; /*l'image reste "cachéé"*/
+  }
+}
+
+
+/* FORME GATEAU*/
+ function formeR(){
+   document.getElementById("demo").innerHTML = "Votre gateau sera en forme de rond";
+ }
+
+ function formeRe(){
+   document.getElementById("demo").innerHTML = "Votre gateau sera en forme de rectangle";
+ }
+ function formeC(){
+   document.getElementById("demo").innerHTML = "Votre gateau aura la forme de coeur";
+ }
+
+
+
+
+/* Prix*/
+function prix1(val){
+    var prix = document.getElementById("main8").value;
+    //var vingts = document.getElementById("seize").value;
+    var amount = prix+val;
+    console.log("prix : " +prix);/* je vois mes erreurs*/
+    //console.log("vingts : " +vingts);
+    console.log("amount : " +amount);
+    document.getElementById("main8").value = amount;
+    document.getElementById("main8").innerHTML = amount +" €";
+}
+
+
+/* reinisialisaton du prix*/
+function reins(){
+  var amount = 0;
+  document.getElementById("main8").value = amount;
+  document.getElementById("main8").innerHTML = amount +" €";
+
+}
